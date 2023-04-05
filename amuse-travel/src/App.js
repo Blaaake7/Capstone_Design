4fa2bacfbe51dfa9ec591a788d1c76631223cb4f
Young JIn <leesu0229@naver.com>
2023-04-05 14:36:03 +0900

    Login, Sing up HTML & CSS

---
+++ amuse-travel/src/App.js
@@ -0,0 +1,12 @@
+import Login from './components/Login';
+import SignUp from './components/Signup';
+
+function App() {
+  return (
+    <div className="App">
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
